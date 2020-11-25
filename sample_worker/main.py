4a41a9e0ac1eda48db10ccdd10d80aa30cc30632
Gabriele Mazzola <gabriele.mazzola@xtreamers.io>
2020-11-25 17:06:56 +0100

    feat: simplify start command and introduce server shutdown

--- sample_worker/main.py
+++ sample_worker/main.py
@@ -1,9 +1,16 @@
 from flask import Flask
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
+from flask import request
 
 app = Flask(__name__)
+
+
+def shutdown_server():
+    func = request.environ.get('werkzeug.server.shutdown')
+    if func is None:
+        raise RuntimeError('Not running with the Werkzeug Server')
+    func()
 
 
 @app.route('/')
@@ -16,8 +23,14 @@ def start():
     chrome_options = Options()
     chrome_options.add_argument("--headless")
     driver = webdriver.Chrome("./chromedriver", chrome_options=chrome_options)
-    driver.get("http://www.python.org")
-    return driver.page_source
+    driver.get("http://www.google.com")
+    return driver.title
+
+
+@app.route('/shutdown')
+def shutdown():
+    shutdown_server()
+    return 'Server shutting down...'
 
 
 if __name__ == '__main__':
