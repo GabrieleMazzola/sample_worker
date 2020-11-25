d7dedbcfe72f3fd0d3d8d0802476008d9e03a058
Gabriele Mazzola <gabriele.mazzola@xtreamers.io>
2020-11-25 15:08:49 +0100

    feat: add start endpoint

--- sample_worker/main.py
+++ sample_worker/main.py
@@ -1,10 +1,23 @@
 from flask import Flask
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+
+@app.route('/start')
+def start():
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    driver = webdriver.Chrome("./chromedriver", chrome_options=chrome_options)
+    driver.get("http://www.python.org")
+    return driver.page_source
 
 
 if __name__ == '__main__':
